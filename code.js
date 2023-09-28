38ba90c3717ecd4f68e0c054cc4c474f77ca27d9
ludwigmalan <116382187+ludwigmalan@users.noreply.github.com>
2023-09-28 13:24:06 +0100

    update

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 let projects=document.getElementById('main')
-let files=['calculator','grapher','gravity simulator','mandelbrot','memory_game','particles','pezu_tower','sound_vissualizer','multigon_render','toInfiniteDecimalPlaces']
+let files=['calculator','grapher','gravity simulator','mandelbrot','memory_game','particles','pezu_tower','sound_vissualizer','multigon_render','toInfiniteDecimalPlaces','spiderman']
 for(let i=0;i<files.length;i++){
     if(files[i]!='node_modules'){
     projects.innerHTML=projects.innerHTML+`<a class="project" href="${files[i]}/index.html">${files[i]}</a>`
